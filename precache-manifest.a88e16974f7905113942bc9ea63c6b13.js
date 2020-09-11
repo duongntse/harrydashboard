2d71b125a4d5ef3498d92918da3247b49eef5bc5
harry <duongntse@gmail.com>
2020-09-11 14:39:26 +0700

    Updates

---
+++ precache-manifest.a88e16974f7905113942bc9ea63c6b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf948a92e6183809eec6f5f3043f7a",
+    "url": "/harrydashboard/index.html"
+  },
+  {
+    "revision": "c6fb1afeb2df6f08aac8",
+    "url": "/harrydashboard/static/css/main.65b3cfc5.chunk.css"
+  },
+  {
+    "revision": "8af20d39342faae0c43b",
+    "url": "/harrydashboard/static/js/2.021bffae.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/harrydashboard/static/js/2.021bffae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fb1afeb2df6f08aac8",
+    "url": "/harrydashboard/static/js/main.4f18563c.chunk.js"
+  },
+  {
+    "revision": "e5c4a8abc665b6f1c53a",
+    "url": "/harrydashboard/static/js/runtime-main.94703507.js"
+  }
+]);
